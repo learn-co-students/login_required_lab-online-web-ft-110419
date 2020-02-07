d07a732c68b6f927016e0ad2745a8fa574092d7e
ncaudill27 <nelson.a.caudill@gmail.com>
2020-02-06 19:55:50 -0500

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,12 @@
+class SecretsController < ApplicationController
+
+  def show
+    redirect_to login_path unless logged_in?
+    current_user
+    # if logged_in?
+    #   current_user
+    # else
+    #   redirect_to login_path
+    # end
+  end
+end
